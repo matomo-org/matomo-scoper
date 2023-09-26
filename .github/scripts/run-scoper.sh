5d9f7c219d7aed4bcb32fd199608bf0817a7672a
diosmosis <diosmosis@users.noreply.github.com>
2023-09-26 06:47:20 -0700

    composer install matomo-scoper before trying to run

--- .github/scripts/run-scoper.sh
+++ .github/scripts/run-scoper.sh
@@ -2,4 +2,11 @@
 
 set -e
 
+# setup matomo-scoper (after matomo tests action sets up PHP, etc.)
+cd ..
+
+composer install # TODO: cache the composer directory for the root repo?
+
+cd matomo
+
 ../bin/matomo-scoper scope -y --composer-path=composer .
