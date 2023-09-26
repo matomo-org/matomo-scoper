799bb083a98d28c0c626d73c5ace29404c0f693a
diosmosis <diosmosis@users.noreply.github.com>
2023-09-26 07:00:14 -0700

    try to use php 8 for matomo-scoper

--- .github/scripts/run-scoper.sh
+++ .github/scripts/run-scoper.sh
@@ -5,8 +5,10 @@ set -e
 # setup matomo-scoper (after matomo tests action sets up PHP, etc.)
 cd ..
 
-composer install # TODO: cache the composer directory for the root repo?
+which php
+
+php8-cli $(which composer) install # TODO: cache the composer directory for the root repo?
 
 cd matomo
 
-../bin/matomo-scoper scope -y --composer-path=composer .
+php8-cli ../bin/matomo-scoper scope -y --composer-path=composer .
