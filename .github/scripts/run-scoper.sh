7bc3533c17457aa553a288387badb34c4e742a67
diosmosis <diosmosis@users.noreply.github.com>
2023-09-28 10:59:53 -0700

    remove TODO

--- .github/scripts/run-scoper.sh
+++ .github/scripts/run-scoper.sh
@@ -5,7 +5,7 @@ set -e
 # setup matomo-scoper (after matomo tests action sets up PHP, etc.)
 cd ..
 
-php8.2 $(which composer) install # TODO: cache the composer directory for the root repo?
+php8.2 $(which composer) install
 
 cd matomo
 
