b65695b5696a28f763b471b082dfd56c57595050
diosmosis <diosmosis@users.noreply.github.com>
2023-10-02 18:42:48 -0700

    in plugin tests make sure directory layout matches what the matomo github tests action expects

--- .github/scripts/run-scoper.sh
+++ .github/scripts/run-scoper.sh
@@ -7,15 +7,17 @@ cd ..
 
 php8.2 $(which composer) install
 
-MATOMO_SCOPER_PATH="$(pwd)/bin/matomo-scoper"
-
 if [ "$PLUGIN_NAME" != "" ]; then
+  MATOMO_SCOPER_PATH="$(pwd)/matomo-scoper/bin/matomo-scoper"
+
   if [ "$PLUGIN_NAME" = "GoogleAnalyticsImporter" ]; then
     cp tests/resources/googleanalyticsimporter-scoper.inc.php matomo/plugins/GoogleAnalyticsImporter/scoper.inc.php
   fi
 
   cd "matomo/plugins/$PLUGIN_NAME"
 else
+  MATOMO_SCOPER_PATH="$(pwd)/bin/matomo-scoper"
+
   cd matomo
 fi
 
