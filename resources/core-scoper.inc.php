98ac81ed773056be5580bb9fd03de619b204a200
diosmosis <diosmosis@users.noreply.github.com>
2024-03-04 17:01:24 -0800

    make sure to prefix other PEAR classes

--- resources/core-scoper.inc.php
+++ resources/core-scoper.inc.php
@@ -84,7 +84,10 @@ return [
                 return $content;
             }
 
-            $content = preg_replace('/([^\\\\A-Za-z0-9_])\\\\PEAR_/', '$1\\Matomo\\Dependencies\\PEAR_', $content);
+            $content = preg_replace('/([^\\\\A-Za-z0-9_])\\\\PEAR/', '$1\\Matomo\\Dependencies\\PEAR', $content);
+            $content = preg_replace('/([^\\\\A-Za-z0-9_])\\\\Archive_Tar/', '$1\\Matomo\\Dependencies\\Archive_Tar', $content);
+            $content = preg_replace('/([^\\\\A-Za-z0-9_])\\\\Console_Getopt/', '$1\\Matomo\\Dependencies\\Console_Getopt', $content);
+            $content = preg_replace('/([^\\\\A-Za-z0-9_])\\\\OS_Guess/', '$1\\Matomo\\Dependencies\\OS_Guess', $content);
             return $content;
         },
 
